f0f600c0b470d68e1125f7f06aebc59ae9c51022
Rudresh Patel <rudreshpatel504@gmail.com>
2025-08-04 21:23:40 +0530

    home screen designing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,9 @@ android {
             )
         }
     }
+    buildFeatures {
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -48,4 +51,6 @@ dependencies {
 
     //lotty animations
     implementation(libs.lottie)
+
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
 }
