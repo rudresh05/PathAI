26c64a0e0ab06462d2681053e645b15b2473a12a
Rudresh Patel <rudreshpatel504@gmail.com>
2025-09-04 19:05:42 +0530

    App with Navigation Drawer

--- app/src/main/java/com/rudresh05/pathai/ui/MainActivity.kt
+++ app/src/main/java/com/rudresh05/pathai/ui/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.rudresh05.pathai.ui
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import com.rudresh05.pathai.R
-import com.rudresh05.pathai.adapters.VideoAdapter
+
 import com.rudresh05.pathai.databinding.ActivityMainBinding
 import com.rudresh05.pathai.fragments.CoursesFragment
 import com.rudresh05.pathai.fragments.HomeFragment
@@ -14,47 +19,61 @@ import com.rudresh05.pathai.fragments.QuizFragment
 import com.rudresh05.pathai.fragments.SearchFragment
 import com.rudresh05.pathai.models.MainViewModel
 
+
 class MainActivity : AppCompatActivity() {
 
     private val mainViewModel: MainViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
-    private lateinit var videoAdapter: VideoAdapter // Adapter ko bahar define karo
+//    private lateinit var videoAdapter: VideoAdapter // Adapter ko bahar define karo
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         replaceFragment(HomeFragment())
 
-binding.bottomNavigationView.setOnItemSelectedListener {item ->
-    when(item.itemId){
-        R.id.homeId -> {
-            replaceFragment(HomeFragment())
-            true
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeId -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+
+                R.id.searchId -> {
+                    replaceFragment(SearchFragment())
+                    true
+                }
+
+                R.id.courseId -> {
+                    replaceFragment(CoursesFragment())
+                    true
+                }
+
+                R.id.quizId -> {
+                    replaceFragment(QuizFragment())
+                    true
+                }
+
+                R.id.profileId -> {
+//            binding.drawerLayout.openDrawer(GravityCompat.START)
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+
+                else -> false
+            }
         }
-        R.id.searchId -> {
-           replaceFragment(SearchFragment())
-            true
+
+        // drawer logic
+        binding.drawerToggleBtn.setOnClickListener {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
         }
-        R.id.courseId -> {
-            replaceFragment(CoursesFragment())
-            true
-        }
-        R.id.quizId -> {
-            replaceFragment(QuizFragment())
-            true
-        }
-        R.id.profileId -> {
-            replaceFragment(ProfileFragment())
-            true
-        }
-        else -> false
-    }
+
     }
 
-}
 
     private fun replaceFragment(frag: Fragment) {
         val support = supportFragmentManager
@@ -63,3 +82,5 @@ binding.bottomNavigationView.setOnItemSelectedListener {item ->
         trans.commit()
     }
 }
+
+
