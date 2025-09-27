887a886ba2f70ed5f340b2552636ca914b4abb29
Rudresh Patel <rudreshpatel504@gmail.com>
2025-09-27 16:58:32 +0530

    Fix Image Slider in Home Fragment

--- app/src/main/java/com/rudresh05/pathai/fragments/HomeFragment.kt
+++ app/src/main/java/com/rudresh05/pathai/fragments/HomeFragment.kt
@@ -35,9 +35,13 @@ class HomeFragment : Fragment() {
 // imageList.add(SlideModel("String Url" or R.drawable)
 // imageList.add(SlideModel("String Url" or R.drawable, "title") You can add title
 
-        imageList.add(SlideModel("https://bit.ly/2YoJ77H", ScaleTypes.CENTER_CROP))
-        imageList.add(SlideModel("https://bit.ly/2BteuF2", ScaleTypes.CENTER_CROP))
-        imageList.add(SlideModel("https://bit.ly/3fLJf72", ScaleTypes.CENTER_CROP))
+        imageList.add(SlideModel("https://www.bluekraft.in/wp-content/uploads/2025/03/what-is-ai-1.jpg", ScaleTypes.CENTER_CROP))
+        imageList.add(SlideModel("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSALsRWjeg7Gqe-nFsTKOPMM13xnSOYVQtpexc2Tmf_VYIlxazfPHtPrLiiPo_NNemcA0&usqp=CAU", ScaleTypes.CENTER_CROP))
+        imageList.add(SlideModel("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQrPtduF5CJ3mK344eTZ53hspstXFJ-iEskA&s", ScaleTypes.CENTER_CROP))
+        imageList.add(SlideModel("https://blog.cdn.cmarix.com/blog/wp-content/uploads/2021/06/8-Killer-Kotlin-Tips-For-Android-App-Developers.png", ScaleTypes.CENTER_CROP))
+        imageList.add(SlideModel("https://www.aress.com/assets/images/inner-headers/android-app-header.webp", ScaleTypes.CENTER_CROP))
+        imageList.add(SlideModel("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiEjBilP-PBEbL7NAsVh5jU2PEYPgaGhh8-g&s", ScaleTypes.CENTER_CROP))
+        imageList.add(SlideModel("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4qoydCWxsFRz69Knzr-NX8B3rCaQYyEMtxg&s", ScaleTypes.CENTER_CROP))
 
         val imageSlider =binding.imageSlider
         imageSlider.setImageList(imageList)
