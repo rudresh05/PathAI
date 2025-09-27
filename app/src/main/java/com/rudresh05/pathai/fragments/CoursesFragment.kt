66c66d7edfe4c347f6000390a9ca2dd6e4fbf153
Rudresh Patel <rudreshpatel504@gmail.com>
2025-09-28 00:42:28 +0530

    fix back crash of web view

--- app/src/main/java/com/rudresh05/pathai/fragments/CoursesFragment.kt
+++ app/src/main/java/com/rudresh05/pathai/fragments/CoursesFragment.kt
@@ -1,5 +1,6 @@
 package com.rudresh05.pathai.fragments
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebViewClient
 import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.recyclerview.widget.RecyclerView
 import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.models.SlideModel
@@ -18,6 +20,7 @@ import com.rudresh05.pathai.adapters.CourseAdapter
 import com.rudresh05.pathai.databinding.FragmentCoursesBinding
 import com.rudresh05.pathai.databinding.FragmentHomeBinding
 import com.rudresh05.pathai.models.dataModels.Course
+import androidx.core.view.isVisible
 
 class CoursesFragment : Fragment() {
     private lateinit var binding: FragmentCoursesBinding
@@ -37,6 +40,7 @@ class CoursesFragment : Fragment() {
     }
 
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -46,33 +50,48 @@ class CoursesFragment : Fragment() {
             image = "https://corizo.in/wp-content/uploads/2024/11/Android-logo.jpg",
             content="https://developer.android.com/get-started/overview"))
         clist.add(Course(id = "2",
+            title = "DSA",
+            image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2tmKCyWGcC5wCCSGkrk6xYd0tiqysvrIsoQ&s",
+            content="https://geeksforgeeks.org/dsa/dsa-tutorial-learn-data-structures-and-algorithms/"))
+        clist.add(Course(id = "3",
             title = "HTML",
             image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDxlqmTPx1zm4lpBc4gSKACq8L85CotcVPnA&s",
             content = "https://www.geeksforgeeks.org/html/html-tutorial/"))
-        clist.add(Course(id = "3",
+        clist.add(Course(id = "4",
             title = "CSS",
             image = "https://colorlib.com/wp/wp-content/uploads/sites/2/creative-css3-tutorials.jpg",
             content = "https://www.geeksforgeeks.org/css/css-tutorial/"))
-        clist.add(Course(id = "4",
+        clist.add(Course(id = "5",
             title = "JAVASCRIPT",
             image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyVIqAglaNUpkS6q5817KhYfBs72Kyejui7w&s",
             content = "https://www.geeksforgeeks.org/javascript/javascript-tutorial/"))
-        clist.add(Course(id = "5",
+        clist.add(Course(id = "6",
             title = "JAVA",
             image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXeNvpYl62B2Iohxp-EO4AFQgE2uRJmNebSw&s",
             content = "https://docs.oracle.com/javase/tutorial/java/index.html"))
-        clist.add(Course(id = "6",
+        clist.add(Course(id = "7",
             title = "REACT",
             image = "https://i.pinimg.com/736x/5c/7b/53/5c7b53a7be1dd267f24f7559584d1345.jpg",
             content = "https://react.dev/learn"))
 
+        clist.add(Course(id = "8",
+            title = "CYBER",
+            image = "https://assets.weforum.org/article/image/5Mvrz-vmTr62JXqvm4BDHJOM2tHyErG6f6KFSvYb2vM.jpg",
+            content = "https://www.geeksforgeeks.org/ethical-hacking/cyber-security-tutorial/"))
+
         var recyclerView = binding.recentActivityRecycler
         var adapter = CourseAdapter(requireContext(),clist){course ->
-            Toast.makeText(requireContext(),"Course Clicked ${course.title}", Toast.LENGTH_LONG).show()
+           // Toast.makeText(requireContext(),"Course Clicked ${course.title}", Toast.LENGTH_LONG).show()
 
             // implement web view here
             val webView = binding.courseWebView
-            webView.settings.javaScriptEnabled = true
+            binding.courseWebView.settings.apply {
+                javaScriptEnabled = true           // JavaScript enable
+                builtInZoomControls = true         // Zoom buttons enable
+                displayZoomControls = false        // Default zoom buttons hide (optional, pinch still kaam karega)
+                useWideViewPort = true             // Content fit screen properly
+                loadWithOverviewMode = true        // Fit content to screen
+            }
 
             webView.webViewClient = WebViewClient()
             webView.visibility = View.VISIBLE
@@ -93,11 +112,17 @@ class CoursesFragment : Fragment() {
 
         var cateRecycler = binding.categoryRecyclerView
         var catAdapter = CategoryAdapter(requireContext(),clist){course ->
-            Toast.makeText(requireContext(), "item click ${course.title}", Toast.LENGTH_SHORT).show()
+           // Toast.makeText(requireContext(), "item click ${course.title}", Toast.LENGTH_SHORT).show()
 
             // implement web view here
             val webView = binding.courseWebView
-            webView.settings.javaScriptEnabled = true
+            binding.courseWebView.settings.apply {
+                javaScriptEnabled = true           // JavaScript enable
+                builtInZoomControls = true         // Zoom buttons enable
+                displayZoomControls = false        // Default zoom buttons hide (optional, pinch still kaam karega)
+                useWideViewPort = true             // Content fit screen properly
+                loadWithOverviewMode = true        // Fit content to screen
+            }
 
             webView.webViewClient = WebViewClient()
             webView.visibility = View.VISIBLE
@@ -113,6 +138,20 @@ class CoursesFragment : Fragment() {
 
 
             binding.courseWebView.loadUrl(course.content ?: "https://www.geeksforgeeks.org/")
+
+
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner){
+            if(binding.courseWebView.isVisible){
+                binding.courseWebView.visibility = View.VISIBLE
+                binding.categoryRecyclerView.visibility = View.VISIBLE
+                binding.recentActivityRecycler.visibility = View.VISIBLE
+                binding.linear1.visibility = View.VISIBLE
+                binding.linear2.visibility = View.VISIBLE
+                binding.cardHeader.visibility = View.VISIBLE
+                binding.editText.visibility = View.VISIBLE
+                binding.ivLogo.visibility = View.VISIBLE
+            }
         }
         cateRecycler.adapter = catAdapter
 
