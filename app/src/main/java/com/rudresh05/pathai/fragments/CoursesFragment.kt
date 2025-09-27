e132333b416e45cde0b6b3e294e304c2142d5bdd
Rudresh Patel <rudreshpatel504@gmail.com>
2025-09-28 00:04:09 +0530

    Adding content in category and recent recycler view and preview in web view

--- app/src/main/java/com/rudresh05/pathai/fragments/CoursesFragment.kt
+++ app/src/main/java/com/rudresh05/pathai/fragments/CoursesFragment.kt
@@ -1,11 +1,13 @@
 package com.rudresh05.pathai.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.denzcoskun.imageslider.constants.ScaleTypes
@@ -39,28 +41,86 @@ class CoursesFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         var clist = ArrayList<Course>()
-        clist.add(Course(id = "1",title = "ANDROID", image = "https://corizo.in/wp-content/uploads/2024/11/Android-logo.jpg"))
-        clist.add(Course(id = "2",title = "HTML", image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDxlqmTPx1zm4lpBc4gSKACq8L85CotcVPnA&s"))
-        clist.add(Course(id = "3",title = "CSS", image = "https://colorlib.com/wp/wp-content/uploads/sites/2/creative-css3-tutorials.jpg"))
-        clist.add(Course(id = "4",title = "JAVASCRIPT", image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyVIqAglaNUpkS6q5817KhYfBs72Kyejui7w&s"))
-        clist.add(Course(id = "5",title = "JAVA", image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXeNvpYl62B2Iohxp-EO4AFQgE2uRJmNebSw&s"))
-        clist.add(Course(id = "6",title = "REACT", image = "https://i.pinimg.com/736x/5c/7b/53/5c7b53a7be1dd267f24f7559584d1345.jpg"))
+        clist.add(Course(id = "1",
+            title = "ANDROID",
+            image = "https://corizo.in/wp-content/uploads/2024/11/Android-logo.jpg",
+            content="https://developer.android.com/get-started/overview"))
+        clist.add(Course(id = "2",
+            title = "HTML",
+            image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDxlqmTPx1zm4lpBc4gSKACq8L85CotcVPnA&s",
+            content = "https://www.geeksforgeeks.org/html/html-tutorial/"))
+        clist.add(Course(id = "3",
+            title = "CSS",
+            image = "https://colorlib.com/wp/wp-content/uploads/sites/2/creative-css3-tutorials.jpg",
+            content = "https://www.geeksforgeeks.org/css/css-tutorial/"))
+        clist.add(Course(id = "4",
+            title = "JAVASCRIPT",
+            image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyVIqAglaNUpkS6q5817KhYfBs72Kyejui7w&s",
+            content = "https://www.geeksforgeeks.org/javascript/javascript-tutorial/"))
+        clist.add(Course(id = "5",
+            title = "JAVA",
+            image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXeNvpYl62B2Iohxp-EO4AFQgE2uRJmNebSw&s",
+            content = "https://docs.oracle.com/javase/tutorial/java/index.html"))
+        clist.add(Course(id = "6",
+            title = "REACT",
+            image = "https://i.pinimg.com/736x/5c/7b/53/5c7b53a7be1dd267f24f7559584d1345.jpg",
+            content = "https://react.dev/learn"))
 
         var recyclerView = binding.recentActivityRecycler
         var adapter = CourseAdapter(requireContext(),clist){course ->
             Toast.makeText(requireContext(),"Course Clicked ${course.title}", Toast.LENGTH_LONG).show()
+
+            // implement web view here
+            val webView = binding.courseWebView
+            webView.settings.javaScriptEnabled = true
+
+            webView.webViewClient = WebViewClient()
+            webView.visibility = View.VISIBLE
+            binding.categoryRecyclerView.visibility = View.GONE
+            binding.recentActivityRecycler.visibility = View.GONE
+            binding.linear1.visibility = View.GONE
+            binding.linear2.visibility = View.GONE
+            binding.cardHeader.visibility = View.GONE
+            binding.editText.visibility = View.GONE
+            binding.ivLogo.visibility = View.GONE
+            binding.courseWebView.visibility = View.VISIBLE
+
+
+
+            binding.courseWebView.loadUrl(course.content ?: "https://www.geeksforgeeks.org/")
         }
         recyclerView.adapter = adapter
 
         var cateRecycler = binding.categoryRecyclerView
         var catAdapter = CategoryAdapter(requireContext(),clist){course ->
             Toast.makeText(requireContext(), "item click ${course.title}", Toast.LENGTH_SHORT).show()
+
+            // implement web view here
+            val webView = binding.courseWebView
+            webView.settings.javaScriptEnabled = true
+
+            webView.webViewClient = WebViewClient()
+            webView.visibility = View.VISIBLE
+            binding.categoryRecyclerView.visibility = View.GONE
+            binding.recentActivityRecycler.visibility = View.GONE
+            binding.linear1.visibility = View.GONE
+            binding.linear2.visibility = View.GONE
+            binding.cardHeader.visibility = View.GONE
+            binding.editText.visibility = View.GONE
+            binding.ivLogo.visibility = View.GONE
+            binding.courseWebView.visibility = View.VISIBLE
+
+
+
+            binding.courseWebView.loadUrl(course.content ?: "https://www.geeksforgeeks.org/")
         }
         cateRecycler.adapter = catAdapter
 
         binding.categoryRecyclerView.setOnClickListener {
 
             Toast.makeText(requireContext(), "Settings clicked", Toast.LENGTH_SHORT).show()
+
+
         }
     }
 
